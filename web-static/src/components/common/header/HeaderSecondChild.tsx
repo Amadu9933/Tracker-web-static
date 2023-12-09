75e65d45438849fc07ed6f953584904dec1faecb
Amadu <amaduhamzajaarah300@gmail.com>
2023-12-09 14:41:28 -0800

    added the overs

--- web-static/src/components/common/header/HeaderSecondChild.tsx
+++ web-static/src/components/common/header/HeaderSecondChild.tsx
@@ -1,7 +1,8 @@
 // CenterContent.tsx
 import React from 'react';
 import './HeaderSecondChild.css'
-
+import search from '../../../assets/Vector.png'
+import profile from '../../../assets/iconoir_profile-circle.png'
 const CenterContent: React.FC = () => {
   return (
     <div className="container text-black text-center mt-4">
@@ -9,9 +10,9 @@ const CenterContent: React.FC = () => {
       <div className=" first-child    mb-2"><span className='H'>How it works</span> <span className='T'>Track my parcel</span> </div>
       <div className="second-child items-center justify-end">
         
-        <div className="search-icon mr-4 cursor-pointer">🔍</div>
+        <div className="search-icon mr-4 cursor-pointer"><img src={search} alt="search" className="search-icon" /></div>
         <div className="Help pmr-4">Need help?</div>
-        <div className="profile mr-4">👤</div>
+        <div className="profile mr-4"><img src={profile} alt="profile" className="profile-icon" /></div>
         <div>Login</div>
       </div>
       </div>
--- web-static/src/components/common/header/Navbar.tsx
+++ web-static/src/components/common/header/Navbar.tsx
@@ -1,12 +1,13 @@
 // Navbar.tsx
 import React from 'react';
 import './Navbar.css'
+import logo from '../../../assets/Logo.png'
 
 
 const Navbar: React.FC = () => {
   return (
     <nav className="nav bg-black text-black flex justify-between items-center">
-      <div className="text-2xl font-bold ">Logo</div>
+      <div className="text-2xl font-bold "><img src={logo} alt="Logo" className="nav-logo" /></div>
       <div className="flex ">
         <div className="nav-customer mr-4 cursor-pointer">Customer</div>
         <div className="nav-business cursor-pointer">Business Owner</div>
