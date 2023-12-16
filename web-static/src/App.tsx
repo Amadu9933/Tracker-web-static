8856cb6806ca1bf2e5cb16bc57873a76e8760198
Lanrewest <47539871+Lanrewest@users.noreply.github.com>
2023-12-16 19:45:57 +0100

    signup

--- web-static/src/App.tsx
+++ web-static/src/App.tsx
@@ -7,6 +7,7 @@ import Header from './components/common/header/Header';
 import Footer from './components/common/footer/Footer';
 import LastChild from './components/common/header/HeaderLastChild';
 import Hero from './components/common/hero/Hero';
+import CustomerSignUp from './components/pages/customerPages/customerAuthentication/CustomerSignUp'
 import TrackingSection from './components/common/trackingSection/TrackingSection';
 
 const App: React.FC = () => {
@@ -19,6 +20,7 @@ const App: React.FC = () => {
       <Header />
       <LastChild />
       <Hero />
+      <CustomerSignUp/>
       <TrackingSection />
       <Footer />
     
