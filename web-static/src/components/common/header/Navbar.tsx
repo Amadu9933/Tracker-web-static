4c9a82cc74a650fe9909b32f2402b6ed0177bcff
Amadu <amaduhamzajaarah300@gmail.com>
2024-02-01 14:49:35 -0800

    nav bar style done

--- web-static/src/components/common/header/Navbar.tsx
+++ web-static/src/components/common/header/Navbar.tsx
@@ -2,21 +2,22 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../../assets/Logo.png";
+import profileIcon from "../../../assets/iconoir_profile-circle.png";
 
 const Navbar: React.FC = () => {
 	return (
-		<nav className=" flex justify-between items-center px-10">
+		<nav className="flex justify-between items-center px-20 py-6 font-medium text-md border-neutral-400  border ">
 			<div className="text-2xl font-bold ">
 				<img src={logo} alt="Logo" className="nav-logo" />
 			</div>
-			<div className="flex ">
-				<div className="nav-customer mr-4 cursor-pointer">Track my parcel</div>
-				<div className="nav-business cursor-pointer">Ship</div>
-				<div>Logistics solution</div>
-				<div>Integration</div>
-				<div>Need help ?</div>
-				<div>
-					<img src="" alt="" />
+			<div className="flex text-neutral-400  h-9">
+				<div className="nav-customer mr-4">Track my parcel</div>
+				<div className="mr-4 nav-customer ">Ship</div>
+				<div className="mr-4 nav-customer">Logistics solution</div>
+				<div className="mr-4 nav-customer">Integration</div>
+				<div className="mr-4 nav-customer">Need help ?</div>
+				<div className="rounded flex py-2 px-4 border-neutral-400  border mt-0">
+					<img src={profileIcon} alt="" />
 					<button>Login</button>
 				</div>
 			</div>
