373ea5dace7856565aefcdde172f18d88ede8ef4
Amadu <amaduhamzajaarah300@gmail.com>
2024-02-02 10:13:34 -0800

    track

--- web-static/src/components/common/hero/Hero.tsx
+++ web-static/src/components/common/hero/Hero.tsx
@@ -8,6 +8,10 @@ const Hero: React.FC = () => {
 			<h1 className="text-white font-bold text-8xl h-48 mx-72 ">
 				Track your parcel today!
 			</h1>
+			<h4 className="mx-80 text-white  text-2xl mt-9">
+				Get real-time location of your parcel from the comfort of your home with
+				trackerr.
+			</h4>
 		</div>
 	);
 };
