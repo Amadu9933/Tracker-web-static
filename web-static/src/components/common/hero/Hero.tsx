664e4a391ce180ec506eb911b57901823e44a34e
Amadu <amaduhamzajaarah300@gmail.com>
2024-02-02 09:05:17 -0800

    track

--- web-static/src/components/common/hero/Hero.tsx
+++ web-static/src/components/common/hero/Hero.tsx
@@ -1,57 +1,13 @@
-
-import React from 'react';
-import MyButton from '../buttons/Mybutton';
-import './Hero.css'
-import first from '../../../assets/first-image.png'
-import second from '../../../assets/second-image.png'
-import third from '../../../assets/third-image.png'
-import arrow from '../../../assets/Arrow 3.png'
-import fourth from '../../../assets/fourth-image.png'
-import pointer from '../../../assets/Group.png'
-
-
-
-
-
+import React from "react";
+import MyButton from "../buttons/Mybutton";
+import "./Hero.css";
 
 const Hero: React.FC = () => {
+	const handleClick = () => {
+		console.log("Button clicked!");
+	};
 
-    const handleClick = () => {
-        console.log('Button clicked!');
-      };
-      
-
-  return (
-   <div className='hero'>
-    <div className='description-item item'>
-        <h2>Track with confidence</h2>
-        <p>Never loose sleep over your order. Unleash  the </p>
-        <p className='lp'>power of percision with Trackerr-Your personal heros</p>
-        
-        <MyButton
-        onClick={handleClick}
-        label="Get started"
-        state="Primary"
-        size="Small"
-        background="#FF833C"
-      />
-        <img src={pointer} alt="pointer" className="pointer" />
-   </div>
-
-   <div className='image-item item'>
-   <img src={first} alt="first" className="first" />
-   <img src={second} alt="second" className="second" />
-   <img src={third} alt="second" className="third" />
-   <img src={fourth} alt="fourth" className="fourth" />
-   
-   </div>
-
-  
-   
-   <img src={arrow} alt="arrow" className="line" />
-
-   </div>
-  );
+	return <div className="hero h-96 bg-slate-600 m-0"></div>;
 };
 
 export default Hero;
--- web-static/src/components/pages/customerPages/CustomerLandingPage.tsx
+++ web-static/src/components/pages/customerPages/CustomerLandingPage.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "../../../App.css";
 import "../../../index.css";
-import Header from "../../common/header/Header";
+import "../../../components/common/hero/Hero.css";
 import Navbar from "../../common/header/Navbar";
+import Hero from "../../common/hero/Hero";
 
 const CustomerLandingPage: React.FC = () => {
 	return (
 		<div>
-			<Navbar />
+			<header>
+				<Navbar />
+				<Hero />
+			</header>
 		</div>
 	);
 };
