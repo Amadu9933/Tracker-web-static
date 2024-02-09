d51a9940c09649cc6e2b7972746d3b876540679a
Amadu <amaduhamzajaarah300@gmail.com>
2024-02-09 15:45:52 -0800

    something done

--- web-static/src/components/pages/customerPages/CustomerDidYouKnow.tsx
+++ web-static/src/components/pages/customerPages/CustomerDidYouKnow.tsx
@@ -7,17 +7,21 @@ import fourthImage from "../../../assets/fourth-image.png";
 
 const CustomerDidYouKnow: React.FC = () => {
 	return (
-		<section className="h-96 bg-red-200">
+		<section className="h-1/4 ">
 			{/* container  */}
-			<div>
+			<div className="flex justify-between">
 				{/* first item */}
-				<div>
-					<img className="" src={fristImage} alt="" />
-					<img src={thirdImage} alt="" />
+				<div className="column ml-44 w-44">
+					<img className="w-36 h-36 rounded-full" src={fristImage} alt="" />
+					<img
+						className="w-20 h-20 rounded-full mt-64 ml-24"
+						src={thirdImage}
+						alt=""
+					/>
 				</div>
 				{/* second item, text container */}
-				<div>
-					<h1>Did You Know?</h1>
+				<div className="w-1/3 mt-9">
+					<h1 className="mb-7">Did You Know?</h1>
 					<p>
 						Trackerr also offers tracking services using tracking I.Ds generated
 						using other logistics partners such as DHL, GIG, etc. Just enter
@@ -26,9 +30,17 @@ const CustomerDidYouKnow: React.FC = () => {
 				</div>
 
 				{/* third container */}
-				<div>
-					<img src={secondImage} alt="" />
-					<img src={fourthImage} alt="" />
+				<div className="mr-44  w-60">
+					<img
+						className="w-20 h-20 rounded-full -mt-36 ml-40"
+						src={secondImage}
+						alt=""
+					/>
+					<img
+						className="w-36 h-36 rounded-full mt-96"
+						src={fourthImage}
+						alt=""
+					/>
 				</div>
 			</div>
 		</section>
