758506778814c26d12937a2f2faeaf3197bc24a8
Lanrewest <47539871+Lanrewest@users.noreply.github.com>
2024-04-10 23:17:12 +0100

    update

--- web-static/src/App.tsx
+++ web-static/src/App.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import React from 'react';
 import './App.css'
@@ -12,41 +12,17 @@ import ForgetPassword from './components/pages/customerPages/customerAuthenticat
 import CustomerSignUp from './components/pages/customerPages/customerAuthentication/CustomerSignUp';
 import TrackingSection from './components/common/trackingSection/TrackingSection';
 import CardSection from './components/common/cards/CardSection';
-=======
-import React from "react";
-import "./App.css";
-import "./index.css";
-import CustomerLandingPage from "./components/pages/customerPages/CustomerLandingPage";
-import CustomerTrackingDetailLayout from "./components/pages/customerPages/CustomerTrackingDetails/CustomerTrackingDetailLayout";
->>>>>>> 5ecf52f2525662839bf56124f94a75d0fdb97853
+
 
 const App: React.FC = () => {
 	return (
 		<div className="App ">
 			{/* Landing page */}
 
-<<<<<<< HEAD
-  return (
-    <div className="App ">
-      
-
-      <Header />
-      <LastChild />
-      <Hero />
-      <CustomerSignUp/>
-      <ForgetPassword/>
-      <TrackingSection />
-      <CardSection/>
-      <Footer />
-    
-    </div>
-  );
-=======
 			<CustomerLandingPage />
 			<CustomerTrackingDetailLayout />
 		</div>
 	);
->>>>>>> 5ecf52f2525662839bf56124f94a75d0fdb97853
 };
 
 export default App;
