1611aa6ab0e3e77f15376148a1bfa63d5364ea96
Lanrewest <47539871+Lanrewest@users.noreply.github.com>
2024-04-12 17:50:35 +0100

    loginpage

--- web-static/src/components/pages/customerPages/BusLogPages/LoginPage.tsx
+++ web-static/src/components/pages/customerPages/BusLogPages/LoginPage.tsx
@@ -1,44 +1,69 @@
-import React from 'react';
-import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import React, { useState } from 'react';
+import { FormControlLabel, Radio, RadioGroup, IconButton, InputAdornment, TextField } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import './LoginPage.css'; 
 
 const LoginPage: React.FC = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="login-container">
       <div className="image-container">
         {/* Your image goes here */}
-        <img src="LoginImage.png" alt="Login" className="login-image" />
+        <img src="./src/assets/LoginImage.png" alt="Login" className="login-image" />
       </div>
       <div>
-      <div className="form-container flex-col">
-        {/* Label with arrow icon */}
-        <div className="welcome-back">
-          <ArrowBackIcon /> Welcome Back!
+        <div className="form-container flex-col">
+          {/* Label with arrow icon */}
+          <div className="welcome-back">
+            <ArrowBackIcon /> Welcome Back!
+          </div>
+          <div className='radio-group'>
+            <RadioGroup aria-label="userType" defaultValue="businessOwner" >
+              <FormControlLabel
+                value="businessOwner"
+                control={<Radio />}
+                label="Business Owner"
+                className="radio-label"
+              />
+              <FormControlLabel
+                value="logisticsPartner"
+                control={<Radio />}
+                label="Logistics Partner"
+                className="radio-label"
+              />
+            </RadioGroup>
+          </div>
+          <p>Sign In</p>
+          <div>
+            <form className="login-form">
+              <input type="text" placeholder="janedoe@gmail.com" className="input-field" />
+              <TextField
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                className="input-field"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handlePasswordVisibility} edge="end">
+                        {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+              <a href="/forgot-password" className="forget-password">Forget password?</a>
+              <button type="submit" className="submit-button">Sign In</button>
+            </form>
+          </div>
+          <p>Do not have an account? <a href="/signup">Sign Up</a></p>
         </div>
-        <div className='radio-group'>
-        <RadioGroup aria-label="userType" defaultValue="businessOwner" >
-          <FormControlLabel
-            value="businessOwner"
-            control={<Radio />}
-            label="Business Owner"
-          />
-          <FormControlLabel
-            value="logisticsPartner"
-            control={<Radio />}
-            label="Logistics Partner"
-          />
-        </RadioGroup>
-        </div>
-       
-        <div>
-          <form className="login-form">
-          <input type="text" placeholder="Username" className="input-field" />
-          <input type="password" placeholder="Password" className="input-field" />
-          <button type="submit" className="submit-button">Login</button>
-        </form>
-        </div>
-      </div>
       </div>
     </div>
   );
