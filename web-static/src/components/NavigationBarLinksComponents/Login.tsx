6ca702c2d3f0dde64d596fb71e24d552d1115fdb
Lanrewest <47539871+Lanrewest@users.noreply.github.com>
2024-04-20 11:03:54 +0100

    next

--- web-static/src/components/NavigationBarLinksComponents/Login.tsx
+++ web-static/src/components/NavigationBarLinksComponents/Login.tsx
@@ -44,7 +44,7 @@ const Login: React.FC = () => {
           		</div>
 				<div className="w-14 h-7  font-inter  text-lg font-medium leading-7 text-left"><label>Sign In</label></div>	
 				
-            <div className="w-522px h-auto gap-0 py-10">
+            {/* <div className="w-522px h-auto gap-0 py-10">
 			<form >
               <input type="text" placeholder="janedoe@gmail.com" className="mb-10 w-full px-4 py-2  bg-gray-100 rounded "/>
               <TextField
@@ -68,39 +68,34 @@ const Login: React.FC = () => {
 			  </div>
             </form>
 			
-			</div>
-			{/* <div className="w-full max-w-xs">
-  				<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    			<div className="mb-4">
-      			<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-        		Username
-      			</label>
-      			<input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-    			</input>
-				</div>
-    		<div className="mb-6">
-      			<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-        		Password
-      			</label>
-      			<input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-      			<p className="text-red-500 text-xs italic">Please choose a password.</p>
-    			</input>
-				</div>
-    			<div className="flex items-center justify-between">
-      			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        		Sign In
-      			</button>
-      			<a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        		Forgot Password?
-      			</a>
-    		</div>
-  			</form>
-  			<p className="text-center text-gray-500 text-xs">
-    			&copy;2020 Acme Corp. All rights reserved.
-  			</p>
 			</div> */}
+			<div className=" w-522 h-341 top-392 left-730 gap-32">
+  <form className="bg-white rounded px-8 pt-6 pb-8 mb-4">
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2 text-left" htmlFor="username">
+        Email
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+    </div>
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2 text-left" htmlFor="password">
+        Password
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+    
+	  <p className="text-black-500 text-xs italic text-right">Forget Password?</p>
+	  <button className="bg-pink-400 w-full hover:bg-pink-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+		</button>
+		<p>Do not have an account? <Link to="/signup">Sign Up</Link></p>
+    </div>
+    	
+  	</form>
+ 
+</div>
+
           </div>
-          <p>Do not have an account? <Link to="/signup">Sign Up</Link></p>
+         
         </div>
 	</div>
 	);
