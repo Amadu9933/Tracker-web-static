89429d1af9f71e095f6aff2b77a3da564eacefb5
Lanrewest <47539871+Lanrewest@users.noreply.github.com>
2024-04-20 04:07:52 +0100

    update4am

--- web-static/src/components/NavigationBarLinksComponents/Login.tsx
+++ web-static/src/components/NavigationBarLinksComponents/Login.tsx
@@ -14,7 +14,7 @@ const Login: React.FC = () => {
   };
 
 	return (
-	<div className="grid grid-cols-1 sm:grid-cols-12  m-4">
+	<div className="grid grid-cols-1 sm:grid-cols-12  m-4 mt-0 ml-0 mr-0">
   		<div className="rounded-none  col-span-5 h-64  bg-blue-500">
 		  <img
     		src="./src/assets/LoginImage.png"
@@ -67,7 +67,38 @@ const Login: React.FC = () => {
 			  <button type="submit" className="submit-button mt-10">Sign In</button>
 			  </div>
             </form>
+			
 			</div>
+			{/* <div className="w-full max-w-xs">
+  				<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    			<div className="mb-4">
+      			<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        		Username
+      			</label>
+      			<input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    			</input>
+				</div>
+    		<div className="mb-6">
+      			<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        		Password
+      			</label>
+      			<input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      			<p className="text-red-500 text-xs italic">Please choose a password.</p>
+    			</input>
+				</div>
+    			<div className="flex items-center justify-between">
+      			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        		Sign In
+      			</button>
+      			<a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        		Forgot Password?
+      			</a>
+    		</div>
+  			</form>
+  			<p className="text-center text-gray-500 text-xs">
+    			&copy;2020 Acme Corp. All rights reserved.
+  			</p>
+			</div> */}
           </div>
           <p>Do not have an account? <Link to="/signup">Sign Up</Link></p>
         </div>
