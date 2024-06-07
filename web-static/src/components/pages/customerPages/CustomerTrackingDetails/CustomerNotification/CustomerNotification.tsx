53a151f7282fe1abefe8f74b30164fd62c75723c
Amadu <amaduhamzajaarah300@gmail.com>
2024-06-07 07:59:46 -0700

    tracking history

--- web-static/src/components/pages/customerPages/CustomerTrackingDetails/CustomerNotification/CustomerNotification.tsx
+++ web-static/src/components/pages/customerPages/CustomerTrackingDetails/CustomerNotification/CustomerNotification.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import CustomizedTables from "./HistoryTable";
 import BasicTable from "./RecentUpdate";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const CustomerNotification: React.FC = () => {
 	return (
 		<div className="mx-[80px]">
+			<div className="flex">
+				<ArrowBackIcon />
+				<p>Parcel History</p>
+			</div>
+			<div className="border-b-2 border-[#D9E1E7]  mb-2 mt-10 text-left">
+				<p className="text-[#5D5D4C]">Recent update</p>
+			</div>
+			<div></div>
 			<div>
 				<BasicTable />
+			</div>
+			<div className="border-b-2 border-[#D9E1E7] mb-2 mt-10 text-left">
+				<p className="text-[#5D5D4C]">History</p>
 			</div>
 			<div>
 				<CustomizedTables />
