1bcba3e5acc7bcf3c41665bc6c0e179681626a6d
Amadu Hamza <amaduhamzajaarah300@mail.com>
2024-12-15 11:11:05 -0800

    radio button color

--- web-static/src/components/pages/BusinessOwnerPages/auth/createAcount/TabSelector.tsx
+++ web-static/src/components/pages/BusinessOwnerPages/auth/createAcount/TabSelector.tsx
@@ -9,26 +9,28 @@ const TabSelector: React.FC<TabSelectorProps> = ({
 }) => {
   return (
     <div className="text-base  flex justify-left  font-inter text-secondary">
-      <label>
+
+      <label className="flex pl-2 ">
         <input
           type="radio"
           value="business "
           checked={selectedTab === 'business'}
           onChange={() => onTabChange('business')}
-          className={selectedTab === 'business' ? 'bg-black' : ''}
+          className={selectedTab === 'business' ? 'accent-black' : ''}
 
         />
-        Business Owner
+        <p className="pt-0.5 pl-2">Business Owner</p>
       </label>
 
-      <label style={{ marginLeft: '20px' }}>
+      <label style={{ marginLeft: '20px' }} className="flex pl-2 ">
         <input
           type="radio"
           value="logistic"
           checked={selectedTab === 'logistic'}
           onChange={() => onTabChange('logistic')}
+          className="accent-black"
         />
-        Logistic Partner
+        <p className="pt-0.5 pl-2">Logistic Partner</p>
       </label>
     </div>
   );
