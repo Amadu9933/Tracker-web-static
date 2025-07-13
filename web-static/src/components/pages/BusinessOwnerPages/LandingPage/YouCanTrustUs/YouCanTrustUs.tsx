84fd827e5d0d972cc41adc00745da0c28fee5153
Amadu <amaduhamzajaarah300@gmail.com>
2025-07-12 14:47:22 -1200

    align

--- web-static/src/components/pages/BusinessOwnerPages/LandingPage/YouCanTrustUs/YouCanTrustUs.tsx
+++ web-static/src/components/pages/BusinessOwnerPages/LandingPage/YouCanTrustUs/YouCanTrustUs.tsx
@@ -3,8 +3,8 @@ import { avatar } from '../guideSection/assets/index';
 
 const YouCanTrustUs: React.FC = () => {
   return (
-    <div className="bg-[#FFF8EF] flex justify-center item-center ">
-      <div className="w-[40%]">
+    <div className="bg-[#FFF8EF] flex justify-center items-center min-h-screen">
+      <div className="w-[40%] text-center">
         <h2 className="text-3xl font-medium text-secondary pt-24 pb-7">
           You can trust us
         </h2>
@@ -13,23 +13,23 @@ const YouCanTrustUs: React.FC = () => {
           Trackerr to transform their shipping experiennce.
         </p>
 
-        <div className="border-2 border-[#8E9090] border-solid rounded-lg  justify-center items-center">
-          <div className="flex justify-center items-center mb-16">
+        <div className="border-2 border-[#8E9090] border-solid rounded-lg p-6 flex flex-col justify-center items-center">
+          <div className="flex justify-center items-center mb-8">
             <img
               src={avatar}
-              alt="Avata"
-              className="rounded-full -m-16  h-[100px] w-[100px]"
+              alt="Avatar"
+              className="rounded-full h-[100px] w-[100px]"
             />
           </div>
-          <p className="text-sm font-normal text-secondary pb-10 ">
-            "I can't express enough how our business has transformed since i
-            started <br /> using Trackerr. It's been a game-changer in every
-            aspect. The seamless <br /> process of generating id for my
-            customers and connecting with dispatchers <br /> to deliver in
-            real-time is on a 100%.
+          <p className="text-sm font-normal text-secondary text-center">
+            "I can't express enough how our business has transformed since I
+            started using Trackerr. It's been a game-changer in every
+            aspect. The seamless process of generating ID for my
+            customers and connecting with dispatchers to deliver in
+            real-time is on a 100%."
           </p>
         </div>
-        <div className=" justify-center py-12   items-center flex">
+        <div className="flex justify-center py-12 items-center">
           <div className="border-b-2 border-b-primary pb-1  border-solid ">
             <a href="" className="text-primary  text-sm font-medium">
               View All
